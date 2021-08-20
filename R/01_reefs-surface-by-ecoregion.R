48b941a347b19ec3de852a1a29e753c375fbc86d
JWicquart <jeremywicquart@gmail.com>
2021-08-20 14:49:29 +0200

    change datasets used for ecoregions and reef surfaces

--- R/01_reefs-surface-by-ecoregion.R
+++ R/01_reefs-surface-by-ecoregion.R
@@ -6,42 +6,34 @@ sf_use_s2(FALSE) # Switch from S2 to GEOS
 
 # 2. Load and transform Reefs at Risk (RAR) data ----
 
-data_rar <- read_sf("data/14_001_WCMC008_CoralReefs2018_v4_1/01_Data/WCMC008_CoralReef2018_Py_v4_1.shp") %>% 
+data_reefs <- read_sf("data/01_reefs-distribution-wri/reef_500_poly.shp") %>% 
   st_transform(crs = 4326) %>% 
   st_wrap_dateline(options = c("WRAPDATELINE=YES")) %>% 
-  st_make_valid() %>% 
-  st_wrap_dateline(options = c("WRAPDATELINE=YES")) %>% 
-  st_make_valid() %>% 
-  st_transform(crs = "ESRI:54001") %>% 
   st_make_valid()
 
-# 3. Load and transform Veron ecoregions data ----
+# 3. Load and transform Marine Ecoregions of the World (MEOW) data ----
 
-load("data/ecoregions.RData")
-
-ecoregions <- ecoregions %>% 
+data_meow <- read_sf("data/02_marine-ecoregions-of-the-world/Marine_Ecoregions_Of_the_World__MEOW_.shp") %>% 
   st_transform(crs = 4326) %>% 
   st_wrap_dateline(options = c("WRAPDATELINE=YES")) %>% 
-  st_make_valid() %>% 
-  st_wrap_dateline(options = c("WRAPDATELINE=YES")) %>% 
-  st_make_valid() %>% 
-  st_transform(crs = "ESRI:54001") %>% 
   st_make_valid()
 
 # 4. Check if the CRS transformation has worked ---
 
 ggplot() +
-  geom_sf(data = ecoregions) +
-  geom_sf(data = data_rar, col = "red")
+  geom_sf(data = data_meow) +
+  geom_sf(data = data_reefs, col = "red")
 
 # 5. Make the join ----
 
-data_joined <- st_intersection(data_rar, ecoregions) %>% 
+data_surface <- st_intersection(data_reefs, data_meow) %>%  
+  st_transform(crs = "ESRI:54001") %>% 
+  st_make_valid() %>%  
   mutate(area = st_area(.)) %>% 
-  group_by(Ecoregion) %>% 
+  group_by(ECOREGION) %>% 
   summarise(area = sum(area)) %>% 
   st_drop_geometry(.)
 
 # 6. Export the data ----
 
-save(data_joined, file = "data/01_reef-surfaces-by-ecoregion.RData")
+save(data_surface, file = "data/04_reef-surface-by-ecoregion.RData")
