821b74a12bd14ff813e440488f1770282fcf5e72
JWicquart <jeremywicquart@gmail.com>
2022-04-04 15:15:27 +0200

    delete 5 ecoregions with no true coral reefs

--- R/01_reefs-ecoregion.R
+++ R/01_reefs-ecoregion.R
@@ -18,13 +18,46 @@ data_meow <- read_sf("data/02_marine-ecoregions-of-the-world/Marine_Ecoregions_O
   st_wrap_dateline(options = c("WRAPDATELINE=YES")) %>% 
   st_make_valid()
 
-# 4. Check if the CRS transformation has worked ---
+# 4. Load and transform background maps ----
 
-ggplot() +
-  geom_sf(data = data_meow) +
-  geom_sf(data = data_reefs, col = "red")
+data_map <- read_sf("data/00_natural-earth-data/ne_10m_land/ne_10m_land.shp") %>% 
+  st_transform(crs = 4326)
 
-# 5. Make the join to estimate surface by ecoregion ----
+# 5. Extract MEOW containing coral reefs ----
+
+data_meowreefs <- st_filter(data_meow, data_reefs, join = st_intersects)
+
+# 6. Produce maps to check ----
+
+## 6.1 Global map -- 
+
+map_checking <- ggplot() +
+  geom_sf(data = data_meowreefs, fill = "lightblue", alpha = 0.3) +
+  geom_sf(data = data_map)
+
+ggsave("figs/03_meow-with-reefs_global.png", map_checking)
+
+## 6.2 Australia --
+
+map_checking <- ggplot() +
+  geom_sf(data = data_meowreefs, fill = "lightblue", alpha = 0.3) +
+  geom_sf(data = data_map) +
+  geom_sf(data = data_reefs, col = "red") +
+  coord_sf(xlim = c(103, 168), ylim = c(-45, -6))
+
+ggsave("figs/03_meow-with-reefs_australia.png", map_checking)
+
+## 6.3 Brazil --
+
+map_checking <- ggplot() +
+  geom_sf(data = data_meowreefs, fill = "lightblue", alpha = 0.3) +
+  geom_sf(data = data_map) +
+  geom_sf(data = data_reefs, col = "red") +
+  coord_sf(xlim = c(-65, -25), ylim = c(-30, 15))
+
+ggsave("figs/03_meow-with-reefs_brazil.png", map_checking)
+
+# 7. Make the join to estimate surface by ecoregion ----
 
 data_surface <- st_intersection(data_reefs, data_meow) %>%  
   st_transform(crs = "ESRI:54001") %>% 
@@ -33,17 +66,19 @@ data_surface <- st_intersection(data_reefs, data_meow) %>%
   group_by(ECOREGION, ECO_CODE_X) %>% 
   summarise(area = sum(area)) %>% 
   st_drop_geometry(.) %>% 
-  mutate(area = as.numeric(area))
+  mutate(area = as.numeric(area)) %>% 
+  # Remove ecoregions containing not "true" coral reefs
+  filter(!(ECO_CODE_X %in% c(71, 72, 77, 203, 209)))
 
-# 6. Export the data ----
+# 8. Export the data ----
 
 save(data_surface, file = "data/04_reef-surface-by-ecoregion.RData")
 
-# 7. Make the join to extract ecoregions with coral reefs ----
+# 9. Make the join to extract ecoregions with coral reefs ----
 
 data_meowreefs <- left_join(data_surface, data_meow) %>% 
   st_as_sf()
 
-# 8. Export the data ----
+# 10. Export the data ----
 
 save(data_meowreefs, file = "data/04_meow-with-coral-reefs.RData")
